5e2eeffb4f28d32fa545df2fc2cab6f80854433b
Dominee Fromhell <dominee@hell.sk>
2025-06-20 10:39:51 +0200

    syntax fix

--- modules/gtk.nix
+++ modules/gtk.nix
@@ -1,10 +1,12 @@
 { config, pkgs, ... }:
 
 {
-  pkgs.catppuccin-gtk.override {
-    accents = [ "blue" ]; # [ "blue" "flamingo" "green" "lavender" "maroon" "mauve" "peach" "pink" "red" "rosewater" "sapphire" "sky" "teal" "yellow" ]
-    size = "compact"; # [ "standard" "compact" ]
-    tweaks = [ "rimless" "black" ]; # [ "black" "rimless" "normal" ];
-    variant = "mocha"; # [ "latte" "frappe" "macchiato" "mocha" ]
-  }
+  environment.systemPackages = with pkgs; [
+  	(catppuccin-gtk.override {
+    		accents = [ "blue" ]; # [ "blue" "flamingo" "green" "lavender" "maroon" "mauve" "peach" "pink" "red" "rosewater" "sapphire" "sky" "teal" "yellow" ]
+    		size = "compact"; # [ "standard" "compact" ]
+    		tweaks = [ "rimless" "black" ]; # [ "black" "rimless" "normal" ];
+    		variant = "mocha"; # [ "latte" "frappe" "macchiato" "mocha" ]
+  	})
+  ];
 }
